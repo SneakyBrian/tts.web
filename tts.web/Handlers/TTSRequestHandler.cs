d7352188b13d1dd85780f677dfde5824cbcd613c
SneakyBrian <SneakyBrian@users.noreply.github.com>
2014-05-27 23:39:52 +0100

    Initial Import - returns wav format

---
+++ tts.web/Handlers/TTSRequestHandler.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Speech.Synthesis;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.Owin;
+
+namespace tts.web.Handlers
+{
+    public class TTSRequestHandler
+    {
+        public Task HandleRequest(IOwinContext context)
+        {
+            return Task.Run(() =>
+            {
+                var text = context.Request.Query["text"];
+
+                if (text.Length > 140)
+                {
+                    text = text.Substring(0, 140);
+                }
+                
+                var gender = VoiceGender.Female;                
+                Enum.TryParse<VoiceGender>(context.Request.Query["gender"], true, out gender);
+
+                var age = VoiceAge.Adult;
+                Enum.TryParse<VoiceAge>(context.Request.Query["age"], true, out age);
+
+                context.Response.ContentType = "audio/wav";
+
+                using(var synth = new SpeechSynthesizer())
+                {
+                    synth.SelectVoiceByHints(gender, age);
+
+                    using(var stream = new MemoryStream())
+                    {
+                        synth.SetOutputToWaveStream(stream);
+
+                        synth.Speak(text);
+
+                        context.Response.Write(stream.GetBuffer());                    
+                    }
+                }
+            });            
+        }
+    }
+}
---
+++ tts.web/Startup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Owin;
+using tts.web.Handlers;
+
+namespace tts.web
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var reqHandler = new TTSRequestHandler();
+
+            app.Run(reqHandler.HandleRequest);
+        }
+    }
+}

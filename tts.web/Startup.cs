9cb35ffc3a39c4bbf39be3c0b0b28ef561b09a58
SneakyBrian <SneakyBrian@users.noreply.github.com>
2014-05-29 21:15:39 +0100

    Working in local azure environment

--- tts.web/Startup.cs
+++ tts.web/Startup.cs
@@ -14,26 +14,9 @@ namespace tts.web
     {
         public void Configuration(IAppBuilder app)
         {
-            CheckAddBinPath();
-
             var reqHandler = new TTSRequestHandler() { Cache = new FileCache(RoleEnvironment.GetLocalResource("fileCache").RootPath) };
 
             app.Run(reqHandler.HandleRequest);
-        }
-
-        public static void CheckAddBinPath()
-        {
-            // find path to 'bin' folder
-            var binPath = Path.Combine(Environment.GetEnvironmentVariable("RoleRoot") + @"\", @"approot\bin");
-            // get current search path from environment
-            var path = Environment.GetEnvironmentVariable("PATH") ?? "";
-
-            // add 'bin' folder to search path if not already present
-            if (!path.Split(Path.PathSeparator).Contains(binPath, StringComparer.CurrentCultureIgnoreCase))
-            {
-                path = string.Join(Path.PathSeparator.ToString(), new string[] { path, binPath });
-                Environment.SetEnvironmentVariable("PATH", path);
-            }
         }
     }
 }
---
+++ tts.web/indist.cmd
@@ -0,0 +1,4 @@
+copy "%~dps0libmp3lame.32.dll" "%windir%\system32\inetsrv"
+copy "%~dps0libmp3lame.64.dll" "%windir%\system32\inetsrv"
+copy "%~dps0libmp3lame.32.dll" "%windir%\system32"
+copy "%~dps0libmp3lame.64.dll" "%windir%\system32"
